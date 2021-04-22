2beb0f9cecbd29493ca128944d27693a8409b2b8
dcore94 <m.lettere@gmail.com>
2021-04-22 12:58:48 +0000

    added strategy and configurations for oauth2

--- ui/src/auth/applyAuthentication.js
+++ ui/src/auth/applyAuthentication.js
@@ -54,6 +54,7 @@ module.exports = (app, opt) => {
   }
 
   // validate authentication strategy
+  console.log(strategies)
   if (!strategies[options.strategy]) {
     throw new Error(`Authentication strategy ${options.strategy} is not supported!`);
   }
@@ -66,13 +67,13 @@ module.exports = (app, opt) => {
   app.use(accessControlMiddleware(options));
 
   // create authentication endpoints
-  app.post(options.loginPath,
+  /*app.post(options.loginPath,
     bodyParser.urlencoded({ extended: false }),
     async (req, res, next) => {
-      passport.authenticate(options.strategy, {
-          session: false
-        }, (err, user, info) => {
-        if (err) {
+      passport.authenticate(options.strategy, { session : false }, 
+      (err, user, info) => {
+        console.log("Inside login path POST", err, res, next)
+	if (err) {
           return next(err);
         }
         if (!user) {
@@ -89,18 +90,73 @@ module.exports = (app, opt) => {
         next();
       })(req, res, next);
     }
+  );*/
+
+  app.get(options.loginPath,
+      passport.authenticate(options.strategy)
   );
+
+  /*app.get(options.indexPath,
+    (req, res, next) => {
+      console.log("Accessing indexPath with ", req.isAuthenticated(), req.user, req.headers, req.cookies);
+      if(!req.user){
+        res.send(
+           '<html><body><a href="/login">Click to login</a></body></html>'
+	)
+      }else{
+        next();
+      }
+    }
+//	  passport.authenticate(options.strategy)
+  );*/
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  }); 
+
+  app.get('/callback',
+       passport.authenticate(options.strategy, { failureRedirect: options.loginPath, successRedirect: options.indexPath }),
+          function(req, res) {
+            if (options && options.strategySettings && options.strategySettings.roles) {
+              console.log(" options roles "+JSON.stringify(options.strategySettings.roles));
+              console.log(" options user roles "+JSON.stringify(req.user.roles));
+              for(var i=0; i < options.strategySettings.roles.length; i++) {
+		if (req.user.roles && req.user.roles.indexOf(options.strategySettings.roles[i]) >= 0) {
+		  const cookieValue = authentication.getCookieValueFromUser(req, req.user, options);
+		  res.cookie(options.cookieName, JSON.stringify(cookieValue), options.cookie);
+		  console.log("Everything ok in callback redirecting to ", options.indexPath)
+		  //res.redirect(options.indexPath);
+		  return;
+		}
+	     }
+	     console.log(" no roles ");
+	     res.clearCookie(options.cookieName, options.cookie);
+	     res.redirect(options.logoutPath);
+           }
+         }
+    ); 
+
   app.get(options.logoutPath, (req, res) => {
+    console.log("Logging out");
     if (req.user && options.audit) {
       options.audit(`User ${req.user.name} logged out`);
     }
     res.clearCookie(options.cookieName, options.cookie);
-    res.redirect(options.loginPath);
+    console.log("Redirecting to keycloak logout");
+    //res.redirect("http://accounts.dev.d4science.org/auth/realms/d4science/protocol/openid-connect/logout?redirect_uri=http://10.1.151.73:5000/callback");
+    res.redirect(options.indexPath)
   });
+
   app.get('/api/me', (req, res) => {
-    res.send({
-      user: req.user,
-      logoutPath: options.logoutPath
-    })
+    if(req.user){
+      res.send({
+        user: req.user,
+        logoutPath: options.logoutPath
+      })
+    }
   });
 };
---
+++ ui/src/auth/strategies/oauth2auth.js
@@ -0,0 +1,66 @@
+const OAuth2Strategy = require('passport-oauth2');
+
+/* @type IAuthentication */
+const OAuth2Authentication = {
+
+	getStrategy(options) {
+        try {
+            console.log(options.strategySettings.authorizationURL);
+            console.log(options.strategySettings.tokenURL);
+            console.log(options.strategySettings.clientID);
+            console.log(options.strategySettings.clientSecret);
+            console.log(options.strategySettings.callbackURL);
+        return new OAuth2Strategy(
+            {
+                authorizationURL: options.strategySettings.authorizationURL,
+                tokenURL: options.strategySettings.tokenURL,
+                clientID: options.strategySettings.clientID,
+                clientSecret: options.strategySettings.clientSecret,
+                callbackURL: options.strategySettings.callbackURL
+            },
+            function(accessToken, refreshToken, profile, cb) {
+                try {
+		    let buff = new Buffer(accessToken.split(".")[1], 'base64');
+		    let at = JSON.parse(buff.toString('ascii'));
+		    console.log("Access token ", at);
+                    //console.log("Refresh token ", refreshToken);
+                    //console.log("id_token "+JSON.stringify(profile));
+                    if(at && at.resource_access && at.resource_access['conductor-ui'].roles.length > 0){
+		      return cb(null, {
+                        name: at.preferred_username,
+                        displayName: at.name,
+                        email: at.email,
+			refresh_token: refreshToken,
+                        roles: at.resource_access['conductor-ui'].roles
+                      })
+		   }else{
+                     console.log("Should not log in")
+		     cb(null, false, { message : "Unable to log in. Invalid token or unsufficient roles"})
+		   }
+                } catch (e) {
+                    console.log(e)
+                    try {
+                        cb(null, false, e);
+                    } catch (cbe) {
+                        console.log(cbe)
+                    }
+                }
+            }
+        )
+        } catch (err) {
+            console.log(err)
+        }
+	},
+
+	getDefaultCookieSecret(options) {
+           console.log("Accessing default cookie secret",options.cookieSecret)
+	   return null;//options.cookieSecret;
+	},
+
+	getCookieValueFromUser(req, user, options) {
+          console.log("") 
+	  return user;
+	}
+}
+
+module.exports = OAuth2Authentication;
